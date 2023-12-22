899555680895a02739ea2fe54b43a27371dfbf2d
KN <kedrosng@gmail.com>
2023-12-22 14:51:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,22 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from perplexity import Perplexity
 
-"""
-# Welcome to Streamlit!
+perplexity = Perplexity("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")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("My Chatbot")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+    
+user_input = st.text_input("You: ", key="input")
+if user_input:
+    output = perplexity.search(user_input)[0]["text"]
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+for i in range(len(st.session_state["generated"])):
+    st.write(f"You: {st.session_state['past'][i]}")
+    st.write(f"Bot: {st.session_state['generated'][i]}")

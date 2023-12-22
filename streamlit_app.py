03f6fad42cb1d33c1dad48eb61f7587470a28165
KN <kedrosng@gmail.com>
2023-12-22 07:05:07 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,44 @@
 import streamlit as st
-from perplexity import Perplexity
+import requests
 
-perplexity = Perplexity("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")
+# Define the API URL and the headers including the authorization token
+url = "https://api.perplexity.ai/chat/completions"
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Bearer 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"
+}
 
-st.title("My Chatbot")
+# Create a text input box for user input
+user_input = st.text_input("You:", key="user_input")
 
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-
-if "past" not in st.session_state:
-    st.session_state["past"] = []
-    
-user_input = st.text_input("You: ", key="input")
+# When the user presses enter, send the input to the chatbot API
 if user_input:
-    output = perplexity.search(user_input)[0]["text"]
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+    payload = {
+        "model": "mistral-7b-instruct",
+        "messages": [
+            {
+                "role": "system",
+                "content": "Be precise and concise."
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    }
 
-for i in range(len(st.session_state["generated"])):
-    st.write(f"You: {st.session_state['past'][i]}")
-    st.write(f"Bot: {st.session_state['generated'][i]}")
+    # Make the POST request to the API
+    response = requests.post(url, json=payload, headers=headers)
+
+    # If the request is successful, display the response
+    if response.status_code == 200:
+        response_data = response.json()
+        chatbot_response = response_data.get("choices", [{}])[0].get("message", {}).get("content", "")
+        st.text_area("Chatbot:", value=chatbot_response, height=100, key="chatbot_response")
+    else:
+        st.error("Failed to get response from the chatbot API")
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    st.title("Chatbot Interface")

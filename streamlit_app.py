6029ce4bc0a482cdfc54937fed90bd82441566c7
KN <kedrosng@gmail.com>
2023-12-27 06:50:02 +0000

    price table

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,23 @@ import requests
 
 st.set_page_config(page_title="Chat with PPLX", page_icon=":robot:")
 
+price_table = """
+| Model Parameter Count | $/1M input tokens | $/1M output tokens |
+|-----------------------|-------------------|--------------------|
+| 7B                    | $0.07             | $0.28              |
+| 13B                   | $0.14             | $0.56              |
+| 34B                   | $0.35             | $1.40              |
+| 70B                   | $0.70             | $2.80              |
+
+**Online Model Pricing**
+
+| Online Model Parameter Count | $/1000 requests | $/1M output tokens |
+|------------------------------|-----------------|--------------------|
+| 7B                           | $5              | $0.28              |
+| 70B                          | $5              | $2.80              |
+"""
 st.title("Chat with PPLX")
+
 
 url = "https://api.perplexity.ai/chat/completions" 
 headers = {
@@ -54,6 +70,8 @@ chat_placeholder.markdown(chat_history_to_md(st.session_state.chat_history), uns
 user_input = st.text_input("You:", value="", on_change=send_message, key="user_input")
 
 st.text(f"You are chatting with the {selected_model} model.")
+st.sidebar.markdown("## Pricing Information")
+st.sidebar.markdown(price_table)
 
 # Function to clear the chat
 def clear_chat():

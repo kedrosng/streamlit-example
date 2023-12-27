6771382bba17c1ccf22aadee3fd48ddb8429bf16
KN <kedrosng@gmail.com>
2023-12-27 06:09:15 +0000

    update 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="Chat with PPLX", page_icon=":robot:") 
+st.set_page_config(page_title="Chat with PPLX", page_icon=":robot:")
 
 st.title("Chat with PPLX")
 
 url = "https://api.perplexity.ai/chat/completions"
 headers = {
-  "authorization": "Bearer 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"  
+  "authorization": "Bearer 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"
 }
 
 models = ["pplx-7b-chat", "pplx-70b-chat", "pplx-7b-online", "pplx-70b-online",
@@ -16,25 +16,33 @@ models = ["pplx-7b-chat", "pplx-70b-chat", "pplx-7b-online", "pplx-70b-online",
 
 selected_model = st.sidebar.selectbox("Select Model", models)
 
-def ask_chatgpt(prompt, model):
-  payload = {
-    "model": model,
-    "messages": [{"role": "user", "content": prompt}]
-  }
-  
-  response = requests.post(url, json=payload, headers=headers)
-  return response.json()["choices"][0]["message"]["content"]
-
-# Initialize chat history like the Counter example
-if 'chat_history' not in st.session_state:
+# Chat history will be stored in the sidebar
+if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-user_input = st.text_input("You:")  
+def get_response(prompt):
+    payload = {"model": selected_model, "messages": [{"role": "user", "content": prompt}]}
+    response = requests.post(url, json=payload, headers=headers).json()
+    return response["choices"][0]["message"]["content"]
+
+with st.sidebar:
+    for msg in st.session_state.chat_history:
+        st.markdown("**You:** "+msg["user"])
+        st.markdown("**PPLX:** "+msg["PPLX"])
+        
+    st.markdown("")
+    
+    if st.button("Start New Chat"):
+        st.session_state.chat_history = []
+        
+    if st.button("Clear All Chats"):
+        st.session_state.chat_history = []
+        
+user_input = st.text_input("You:", placeholder="Enter message...")
 
 if st.button("Send"):
-  response = ask_chatgpt(user_input, selected_model)
+  response = get_response(user_input)
   st.session_state.chat_history.append({"user": user_input, "PPLX": response})
 
-for message in st.session_state.chat_history:
-  st.markdown("You: " + message["user"]) 
-  st.markdown("PPLX: " + message["PPLX"])
+st.text("")  
+st.text(f"You are chatting with the {selected_model} model.")

161bd63e58fa722da534a55bb994735f053cb2d5
KN <kedrosng@gmail.com>
2023-12-27 05:37:16 +0000

    Add selection options

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ headers = {
   "authorization": "Bearer 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"
 }
 
+models = ["pplx-7b-chat", "pplx-70b-chat", "pplx-7b-online", "pplx-70b-online", "llama-2-70b-chat", "codellama-34b-instruct", "mistral-7b-instruct", "mixtral-8x7b-instruct"]
+selected_model = st.selectbox("Select Model", models)
+
 def ask_chatgpt(prompt, model):
   payload = {
     "model": model,
@@ -18,25 +21,14 @@ def ask_chatgpt(prompt, model):
   return response.json()["choices"][0]["message"]["content"]
 
 st.title("Chat with PPLX")
-
-# Initialize chat history
-if "messages" not in st.session_state:
-  st.session_state.messages = []
-
-# Display chat messages from history
-for message in st.session_state.messages:
-  st.text_area(message["role"] + ":", value=message["content"], disabled=True)
-
-# Model selection
-model = st.selectbox("Select Model", ["pplx-70b-online", "pplx-7b-online", "pplx-70b-chat"])
-
 user_input = st.text_input("You:")
 
 if st.button("Send"):
-  response = ask_chatgpt(user_input, model)
-  
-  # Add user message to chat history
-  st.session_state.messages.append({"role": "You", "content": user_input})
-  
-  # Add assistant response to chat history
-  st.session_state.messages.append({"role": "PPLX", "content": response})
+  response = ask_chatgpt(user_input, selected_model)
+  if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+  st.session_state.chat_history.append({"user": user_input, "PPLX": response})
+
+for message in st.session_state.chat_history:
+  st.text("You: " + message["user"])
+  st.text("PPLX: " + message["PPLX"])

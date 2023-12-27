7a54a4adfbf65da6c366586e5ef18d436d17e7a5
KN <kedrosng@gmail.com>
2023-12-27 06:52:19 +0000

    updated list

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ headers = {
 }
 
 models = [
-    "pplx-70b-chat", "pplx-7b-chat", "pplx-7b-online", "pplx-70b-online",
+    "pplx-7b-online", "pplx-70b-chat", "pplx-7b-chat", "pplx-70b-online",
     "llama-2-70b-chat", "codellama-34b-instruct", "mistral-7b-instruct",
     "mixtral-8x7b-instruct"
 ]

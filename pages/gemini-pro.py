322fc9f81c056d46462b0bb3cea05d06fc07fa84
KN <kedrosng@gmail.com>
2023-12-28 15:10:21 +0000

    Finally Done

--- pages/gemini-pro.py
+++ pages/gemini-pro.py
@@ -1,40 +1,24 @@
-import google.generativeai as genai
 import streamlit as st
 import json
+import google.generativeai as genai
 
-# Function to initialize session state
 def initialize_session_state():
-    return st.session_state.setdefault('api_key', None)
+    if 'api_key' not in st.session_state:
+        st.session_state['api_key'] = "AIzaSyBwibYDUMg8gFiKBvRRJjvJCiLTi6_Er3Q"
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+    if 'chat_placeholder' not in st.session_state:
+        st.session_state['chat_placeholder'] = st.empty()
 
-# Main Streamlit app
-def text_page():
-    st.title("Gemini Pro 🤖")
-
-    # Initialize session state
-    initialize_session_state()
-
-    # Configure API key
-    api_key = "AIzaSyBwibYDUMg8gFiKBvRRJjvJCiLTi6_Er3Q"
-    #api_key = st.sidebar.text_input("Enter your API key:", value=st.session_state.api_key)
-
-    # Check if the API key is provided
-    if not api_key:
-        st.sidebar.error("Please enter your API key.")
-        st.stop()
-    else:
-        # Store the API key in session state
-        st.session_state.api_key = api_key
-
+def configure_ai_model():
+    api_key = st.session_state.api_key
     genai.configure(api_key=api_key)
 
-    
-    # Set up the model configuration options
     temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.6, 0.1)
     top_p = st.sidebar.number_input("Top P", 0.0, 1.0, 1.0, 0.1)
     top_k = st.sidebar.number_input("Top K", 1, 100, 1)
     max_output_tokens = st.sidebar.number_input("Max Output Tokens", 1, 10000, 2048)
 
-    # Set up the model
     generation_config = {
         "temperature": temperature,
         "top_p": top_p,
@@ -42,36 +26,53 @@ def text_page():
         "max_output_tokens": max_output_tokens,
     }
 
-
     safety_settings = "{}"
     safety_settings = json.loads(safety_settings)
-        
-    prompt = st.text_input("Enter your Query:")
-    # Check if the query is provided
-    if not prompt:
-        st.error("Please enter your query.")
-        st.stop()
-
 
     gemini = genai.GenerativeModel(model_name="gemini-pro",
-                                  generation_config=generation_config,
-                                  safety_settings=safety_settings)
-                
+                                   generation_config=generation_config,
+                                   safety_settings=safety_settings)
+    
+    return gemini
 
-    prompt_parts = [prompt]
+def get_response(user_input):
+    gemini = configure_ai_model()
+    prompt_parts = [user_input]
     
     try:
         response = gemini.generate_content(prompt_parts)
-        st.subheader("Gemini:")
-        if response.text:
-            
-            st.write(response.text)
-        else:
-            st.write("No output from Gemini.")
+        return response.text if response.text else "No output from Gemini."
     except Exception as e:
-        st.write(f"An error occurred: {str(e)}")
+        st.error(f"An error occurred: {str(e)}")
+        return "An error occurred while communicating with Gemini Pro."
 
+def send_message():
+    user_input = st.session_state.user_input
+    if user_input:
+        response = get_response(user_input)
+        st.session_state.chat_history.append({"user": user_input, "PPLX": response})
+        st.session_state.chat_placeholder.markdown(chat_history_to_md(st.session_state.chat_history), unsafe_allow_html=True)
+        st.session_state.user_input = ""
 
-# Run the Streamlit app
+def chat_history_to_md(chat_history):
+    markdown_text = ""
+    chat_id = 1
+    for msg in chat_history:
+        markdown_text += f"**#{chat_id} You:** {msg['user']}\n\n"
+        markdown_text += f"**#{chat_id} Gemini Pro:** {msg['PPLX']}\n\n"
+        chat_id += 1
+    return markdown_text
+
+def text_page():
+    st.title("Gemini Pro 🤖")
+    initialize_session_state()
+    chat_placeholder = st.empty()
+    chat_placeholder.markdown(chat_history_to_md(st.session_state.chat_history), unsafe_allow_html=True)
+    st.text_input("You:", value="", on_change=send_message, key="user_input")
+
+    if st.button('Clear Chat'):
+        st.session_state.chat_history = []
+        chat_placeholder.empty()
+
 if __name__ == "__main__":
     text_page()

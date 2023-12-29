0081a7ad82e6551a2c3e7ffad8258fb0948d62c2
KN <kedrosng@gmail.com>
2023-12-29 00:21:04 +0000

    6

--- pages/gemini-pro-full.py
+++ pages/gemini-pro-full.py
@@ -12,7 +12,6 @@ import requests
 
 st.title("Gemini Pro Full (Free)")
 st.set_page_config(
-    page_title="Google Gemini Full (Free)",
     page_icon="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png",
     layout="wide",
 )

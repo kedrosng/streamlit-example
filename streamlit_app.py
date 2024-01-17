56c247ecb78fa1aa9708839a974208860ca5dca7
KN <kedrosng@gmail.com>
2024-01-17 00:28:01 +0000

    remake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="Perplexity AI (Paid)", page_icon=":robot:")
+st.set_page_config(page_title="EnChat", page_icon=":robot:")
 
 
 price_table = """
@@ -19,58 +19,7 @@ price_table = """
 | 7B                           | $5              | $0.28              |
 | 70B                          | $5              | $2.80              |
 """
-st.title("Perplexity AI (Paid)")
-
-
-url = "https://api.perplexity.ai/chat/completions" 
-headers = {
-    "authorization": "Bearer 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"
-}
-
-models = [
-    "pplx-7b-online",
-    "pplx-70b-chat", "pplx-7b-chat", "pplx-70b-online","llama-2-70b-chat", "codellama-34b-instruct", "mistral-7b-instruct","mixtral-8x7b-instruct"
-]
-
-
-
-selected_model = st.sidebar.selectbox("Select Model", models)
-st.text(f"You are chatting with the {selected_model} model.")
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
-
-def get_response(prompt):
-    payload = {"model": selected_model, "messages": [{"role": "user", "content": prompt}]}
-    response = requests.post(url, json=payload, headers=headers).json()
-    return response["choices"][0]["message"]["content"]
-
-# Placeholder for the chat output
-chat_placeholder = st.empty()
-
-def send_message():
-    user_input = st.session_state.user_input
-    if user_input:  # Check if the input is not empty
-        response = get_response(user_input)
-        # Append both user and PPLX messages to the chat history
-        st.session_state.chat_history.append({"user": user_input, "PPLX": response})
-        # Update the chat display
-        chat_placeholder.markdown(chat_history_to_md(st.session_state.chat_history), unsafe_allow_html=True)
-        # Clear the input field
-        st.session_state.user_input = ""
-
-def chat_history_to_md(chat_history):
-    markdown_text = ""
-    chat_id = 1
-    for msg in chat_history:
-        markdown_text += f"**#{chat_id} You:** {msg['user']}\n\n"
-        markdown_text += f"**#{chat_id} ChatBotPPLX:** {msg['PPLX']}\n\n"
-        chat_id += 1
-    return markdown_text
-
-chat_placeholder.markdown(chat_history_to_md(st.session_state.chat_history), unsafe_allow_html=True)
-
-user_input = st.text_input("You:", value="", on_change=send_message, key="user_input")
-
+st.title("Home")
 
 st.sidebar.markdown("## Pricing Information")
 st.sidebar.markdown(price_table)
